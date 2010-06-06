a3266342f98e64fa9cba5f0003019648570cd7d6
Dan Mohl <dmohl@yahoo.com>
2010-06-06 14:51:49 -0500

    Created

---
+++ src/Services/Service1.svc.fs
@@ -0,0 +1,14 @@
+﻿namespace FSharpWcfServiceApplicationTemplate
+
+open System
+
+type Service1() =
+    interface IService1 with
+        member x.GetData value =
+            sprintf "%A" value
+        member x.GetDataUsingDataContract composite =
+            match composite.BoolValue with
+            | true -> composite.StringValue <- 
+                          sprintf "%A%A" composite.StringValue "Suffix"
+            | _ -> "do nothing" |> ignore
+            composite
